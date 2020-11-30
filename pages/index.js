4d14ee8eec1704edb95610221c651ffc9de52d01
René Mäkeler <maekeler@marmalade.de>
2020-11-30 15:37:00 +0100

    Create minimal reproduction for queryParam issue

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
+import { Component } from 'react'
 import Link from 'next/link'
+import Router from 'next/router'
 
-export default function Home() {
-  return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+export default class Home extends Component {
+  static async getInitialProps(ctx) {
+    console.log('ctx.query', ctx.query)
+
+    return { foo: 'bar' }
+  }
+
+  handleButtonClick = () => {
+    const queryString = 'filter%5Bprice_to%5D=103'
+
+    Router.push(`/?${queryString}`, `/?${queryString}`)
+  }
+
+  render() {
+    return (
+      <ul>
+        <li>
+          <button type="button" onClick={this.handleButtonClick}>
+            Navigate with query param
+          </button>
+        </li>
+        <li>
+          <Link href="/a">
+            <a>a</a>
+          </Link>
+        </li>
+      </ul>
+    )
+  }
 }
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
+  server.get('/', (req, res) => {
+    return app.render(req, res, '/', req.query)
   })
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query)
   })
 
   server.all('*', (req, res) => {
